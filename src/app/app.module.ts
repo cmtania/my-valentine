ffa86015c896550869a0e4ac3d803e2e3921250b
tania.dev <taniachristian01@gmail.com>
2025-02-05 21:41:50 +0800

    remove flower component and update package.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FlowerComponent } from './feature/flower/flower.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FlowerComponent
   ],
   imports: [
     BrowserModule,
