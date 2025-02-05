df2859ee134b2a3aabc6e9f68fdfe24e1d3bbdda
tania.dev <taniachristian01@gmail.com>
2025-02-05 21:17:32 +0800

    Update routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+export const routes: Routes = [
+  
+    {
+      path: "home", component: AppComponent
+    },
+    {
+      path: "", redirectTo: "home", pathMatch: "full"
+    },
+    
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
